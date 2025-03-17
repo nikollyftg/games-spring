e4a66e5cd516287a7901c9fad1eca86e732288ac
Nikolly Fernanda <nikollytrombini@gmail.com>
2025-03-17 17:16:37 +0000

    salvando

--- app/src/main/java/application/repository/CategoriaRepository.java
+++ app/src/main/java/application/repository/CategoriaRepository.java
@@ -1,9 +1,8 @@
 package application.repository;
 
-import org.springframework.data.repository.CrudRepository;
-
 import application.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CategoriaRepository extends CrudRepository<Categoria, Long>{
-    
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
