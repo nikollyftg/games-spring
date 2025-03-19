2884326cc9263ba79ecba9d910faf96053b0e137
Nikolly Fernanda <nikollytrombini@gmail.com>
2025-03-19 17:46:27 +0000

    Salvando

--- app/src/main/java/application/repository/PlataformaRepository.java
+++ app/src/main/java/application/repository/PlataformaRepository.java
@@ -1,8 +1,9 @@
 package application.repository;
-
+ 
+import org.springframework.data.repository.CrudRepository;
+ 
 import application.model.Plataforma;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface PlataformaRepository extends JpaRepository<Plataforma, Long> {
+ 
+public interface PlataformaRepository extends CrudRepository<Plataforma, Long> {
+ 
 }
